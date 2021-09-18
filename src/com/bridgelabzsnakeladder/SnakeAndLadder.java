9b49280b39e553d70429c950b2ba043deedb96a0
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-18 20:39:42 +0530

    Added: roll dice and no between 1 to 6

--- src/com/bridgelabzsnakeladder/SnakeAndLadder.java
+++ src/com/bridgelabzsnakeladder/SnakeAndLadder.java
@@ -2,9 +2,20 @@ package com.bridgelabzsnakeladder;
 
 public class SnakeAndLadder {
 
-	public static void main(String[] args) {
-        int position = 0;  //initialization of position
-        System.out.println("Welcome to Snake and ladder Problem");
-}
+	int position =0; //initialize position
+
+    public static void main(String[] args) {
+        SnakeAndLadder snakeLad = new SnakeAndLadder();
+        int diceValue = snakeLad.rollDice(); // call method her and store method return value
+        System.out.println(+diceValue);
+    }
+
+    public int rollDice() {
+        /* Method used Math class
+         * to Generating Random number between 1 to 6
+         */
+        int dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Dice Roll is : " + dice);
+        return dice;	}
 
 }
