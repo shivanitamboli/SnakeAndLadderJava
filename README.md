dfb7af7ddc1cd40fc1ac3d3ae2ac4a5197adf938
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-18 20:36:02 +0530

    Added: Initialization of snake and ladder

---
+++ README.md
@@ -0,0 +1,2 @@
+# SnakeAndLadderJava
+# SnakeAndLadderJava
---
+++ src/com/bridgelabzsnakeladder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabzsnakeladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+        int position = 0;  //initialization of position
+        System.out.println("Welcome to Snake and ladder Problem");
+}
+
+}
